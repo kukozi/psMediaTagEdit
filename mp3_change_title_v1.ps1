a8b27a24b206db64de03109b8fba8e50408cfbf7
Mike G <kukozi47@gmail.com>
2023-04-09 13:37:26 +0300

    initial commit

---
+++ mp3_change_title_v1.ps1
@@ -0,0 +1,14 @@
+﻿clear
+$path = $PWD.Path
+#$taglib = “\taglib-sharp.dll”
+#$taglibpath = $path + $taglib
+$taglibpath= "file:///C:\Users\kukozi\Downloads\Career Paths Psychology Audio\taglib-sharp.dll"
+[System.Reflection.Assembly]::LoadFrom($taglibpath) | out-null
+$files = Get-ChildItem -Path $path | Where-Object {$_.Extension -eq '.mp3'}
+foreach ($filename in $files){
+$media = [TagLib.File]::Create($filename)
+#$title = $media.Tag.Title
+$title = $filename -replace "CP PSYCHOLOGY CD1 Track", "CP Psy"
+$media.Tag.Title = $title
+$media.Save()
+}
